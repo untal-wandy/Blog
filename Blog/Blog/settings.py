721774985bdcf2f90b6720670575bed38b8fd5bf
Wandy Olivares <wandy.olivares@outlook.com>
2023-06-24 20:37:31 -0400

    Mejoras

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['  https://ecc5-205-137-52-54.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = ['   https://b118-205-137-52-172.ngrok-free.app']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
